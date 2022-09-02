31cb76026d394b5c7e15b13eca47d04ea7ff55f4
Joey Chen <32921212+joeyxworks@users.noreply.github.com>
2022-09-02 17:08:56 +0800

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,22 @@
+#import regex module
+import re
+
+#import the mac addresses from a file, form of MAC address should be like 98D8639B7996
+macLines = '/path/to/your/file'
+
+#define the regex pattern in splitting two characters into group, six groups in total
+regex_pattern = r"([0-9a-fA-F]{1,2})([0-9a-fA-F]{1,2})([0-9a-fA-F]{1,2})([0-9a-fA-F]{1,2})([0-9a-fA-F]{1,2})([0-9a-fA-F]{1,2})"
+
+#read content from file
+with open(macLines, 'r') as f:
+	#use for loop to regex-searching in each line
+	for line in f:
+		re_result = re.search(regex_patterm, line)
+		#concatenate groups extracted by regex with ':'
+		mac_addr = re_result.group(1) \
+		+ ':' + re_result.group(2) \
+		+ ':' + re_result.group(3) \
+		+ ':' + re_result.group(4) \
+		+ ':' + re_result.group(5) \
+		+ ':' + re_result.group(6)
+		print(mac_addr)
